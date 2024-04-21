1ea0b26d846f5b7d08aac1f14c3bcc4b3f33cd7c
levi <mcmlevi@outlook.com>
2024-04-21 13:43:28 +0200

    day 4 part 2

--- source/day4.py
+++ source/day4.py
@@ -1,3 +1,5 @@
+"""This module solves day 4 of AOC 2023"""
+
 def get_numbers(line: str):
     """Get all numbers from an given line
 
@@ -14,6 +16,7 @@ def get_numbers(line: str):
     return (winning_numbers, card_numbers)
 
 def part1():
+    """Solve part 1 of day 4"""
     file = open("data/day4.txt", encoding="utf-8")
     sum_of_cards = 0
     for line in file.readlines():
@@ -24,4 +27,30 @@ def part1():
 
     return sum_of_cards
 
+class ScratchCard:
+    """container representing the scratch card
+    """
+    winning_cards = []
+    card_numbers = []
+
+    def __init__(self, line: str) -> None:
+        (self.winning_cards, self.card_numbers) = get_numbers(line)
+
+    def get_number_of_matches(self):
+        """ Get the number of matches from the winning and contained cards"""
+        return len([x for x in self.card_numbers if x in self.winning_cards])
+
+def part2():
+    """Solve part 2 of day 4 of AOC"""
+    file = open("data/day4.txt", encoding="utf-8")
+    cards = [(ScratchCard(line), 1) for line in file.readlines()]
+    for (index, (card, count)) in enumerate(cards):
+        for i in range(1, card.get_number_of_matches() + 1):
+            (next_card, next_card_count) = cards[index + i]
+            cards[index + i] = (next_card, next_card_count + count)
+
+    return sum(count for (_, count) in cards)
+
+
 print(part1())
+print(part2())
